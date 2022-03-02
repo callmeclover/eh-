a9fa11e13b3191917ddd82b53585cb8211390509
MarshyDev <95094165+MarshyDev@users.noreply.github.com>
2022-03-02 13:57:37 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,9 @@
-function bad() {
-document.write("never gonna give you up")
-document.write("never gonna let you down")
+function myFunction() {
+document.write('check dev console (control + shift + i)')
+console.log("never gonna give you up")
+console.log("never gonna let you down")
+console.log("never gonna, run around, and desert you.")
+console.log("never gonna make you cry")
+console.log("never gonna say goodbye")
+console.log("never gonna, tell a lie, and hurt you.")
 }
