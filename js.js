37f681a275572c815109d056104d108352f1aa8a
MarshyDev <95094165+MarshyDev@users.noreply.github.com>
2022-03-02 14:29:29 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,12 +1,9 @@
 function myFunction() {
-document.write('check dev console (control + shift + i)')
 console.log("never gonna give you up")
 console.log("never gonna let you down")
 console.log("never gonna, run around, and desert you.")
 console.log("never gonna make you cry")
 console.log("never gonna say goodbye")
 console.log("never gonna, tell a lie, and hurt you.")
-}
-function a() {
-
+document.write('check dev console (control + shift + i)')
 }
