b329d66c5cc699180205d9aeff7c999c56aee068
MarshyDev <95094165+MarshyDev@users.noreply.github.com>
2022-03-10 11:17:17 -0500

    Update js.js

--- js.js
+++ js.js
@@ -5,5 +5,5 @@ console.log("never gonna, run around, and desert you.")
 console.log("never gonna make you cry")
 console.log("never gonna say goodbye")
 console.log("never gonna, tell a lie, and hurt you.")
-document.write('check dev console (control + shift + i)')
+document.write('check dev console (control + shift + j)')
 }
